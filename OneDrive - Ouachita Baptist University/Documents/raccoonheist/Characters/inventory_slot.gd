a5074b6140cc43008339b275f47984e05b800534
Olivia Marine <ogm043@yahoo.com>
2024-11-07 13:54:40 -0600

    working on inventory and added the animations for the raccoon in costume.: q c g
    git add .

--- OneDrive - Ouachita Baptist University/Documents/raccoonheist/Characters/inventory_slot.gd
+++ OneDrive - Ouachita Baptist University/Documents/raccoonheist/Characters/inventory_slot.gd
@@ -1,6 +1,9 @@
 extends Button
-
-
+var item : Item
+var quantity : int
+@onready var icon : TextureRect = get_node("Icon")
+@onready var quantity_text : Label = get_node("QuantityText")
+var inventory : inventory
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -8,4 +11,16 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	pass
+
+func set_item(new_item : Item):
+	pass
+	
+func add_item():
+	pass
+	
+func remove_item():
+	pass
+	
+func update_quantity_text():
 	pass

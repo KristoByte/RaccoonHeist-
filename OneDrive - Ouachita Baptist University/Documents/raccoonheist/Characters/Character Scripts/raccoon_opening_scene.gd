b3336d102de784ca3615797d148aade5be40099f
Olivia Marine <ogm043@yahoo.com>
2024-11-14 13:59:30 -0600

    working on the details.

--- OneDrive - Ouachita Baptist University/Documents/raccoonheist/Characters/Character Scripts/raccoon_opening_scene.gd
+++ OneDrive - Ouachita Baptist University/Documents/raccoonheist/Characters/Character Scripts/raccoon_opening_scene.gd
@@ -1,17 +1,14 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 200.0
 
 
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("left", "right", "up", "down")
-
-	# Get the input direction and handle the movement/deceleration.
+	#Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	velocity = direction * SPEED
-
-
 	move_and_slide()
 	
 func _process(delta):

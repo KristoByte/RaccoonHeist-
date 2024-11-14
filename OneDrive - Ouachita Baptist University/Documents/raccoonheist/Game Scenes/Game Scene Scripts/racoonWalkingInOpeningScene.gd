32568a0867fecccc81141acd9166683685399c59
Olivia Marine <ogm043@yahoo.com>
2024-11-14 13:44:51 -0600

    got the change scene after raccoon talking.

--- OneDrive - Ouachita Baptist University/Documents/raccoonheist/Game Scenes/Game Scene Scripts/racoonWalkingInOpeningScene.gd
+++ OneDrive - Ouachita Baptist University/Documents/raccoonheist/Game Scenes/Game Scene Scripts/racoonWalkingInOpeningScene.gd
@@ -3,7 +3,7 @@ extends PathFollow2D
 var speed = 0.15
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	Dialogic.signal_event.connect(_on_dialogic_signal)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -13,6 +13,11 @@ func _process(delta):
 		$CharacterBody2D/AnimatedSprite2D.play("idle forward")
 		if Input.is_action_just_pressed("talk"):
 			Dialogic.start("raccoonTalkingToPlayer")
+		
 			
 	if progress_ratio < 1:
 		$CharacterBody2D/AnimatedSprite2D.play("idle right")
+
+func _on_dialogic_signal(argument: String):
+	if argument == "change_scene":
+		get_tree().change_scene_to_file("res://Game Scenes/MissingParts.tscn")

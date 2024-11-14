e51d37d710a426b916130af34a6320321cd79054
Olivia Marine <ogm043@yahoo.com>
2024-11-14 13:18:40 -0600

    working on the items for the inventory and failing fr.

--- OneDrive - Ouachita Baptist University/Documents/raccoonheist/Characters/inventory.gd
+++ OneDrive - Ouachita Baptist University/Documents/raccoonheist/Characters/inventory.gd
@@ -3,16 +3,16 @@ extends Node2D
 
 @onready var window : Panel = get_node("Inventory Window")
 @onready var info_text : Label = get_node("Inventory Window/InfoText")
-#@export var starter_items : Array[Item]
+@export var starter_items : Array[Item]
 var slots : Array[InventorySlot]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	toggle_window(false)
-	#for child in get_node("Inventory Window/SlotContainer").get_children():
-		#slots.append(child)
-		#child.set_item(null)
-		#child.inventory = self
+	for child in get_node("Inventory Window/SlotContainer").get_children():
+		slots.append(child)
+		child.set_item(null)
+		child.inventory = self
 	#for item in starter_items:
 		#add_item(item)
 
@@ -30,50 +30,49 @@ func toggle_window (open: bool):
 	else:
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	
-#func on_give_player_item (item: Item, amount: int):
-	#pass
+func on_give_player_item (item: Item, amount: int):
+	pass
 	
-#func add_item (item: Item):
-	#var slot = get_slot_to_add(item)
-	#
-	#if slot == null:
-		#return
-	#
-	#if slot.item == null:
-		#slot.set_item(item)
-	#elif slot.item == item:
-		#slot.add_item()
-#
-#func remove_item (item: Item):
-	#var slot = get_slot_to_remove(item)
-	#
-	#if slot == null or slot.item == item:
-		#return
-	#
-	#slot.remove_item()
-#
-#func get_slot_to_add (item: Item) -> InventorySlot:
-	#for slot in slots:
-		#if slot.item == item and slot.quantity < item.max_stack_size:
-			#return slot
-	#for slot in slots:
-		#if slot.item == null:
-			#return slot
-	#
-	#return null
-#
-#func get_slot_to_remove (item: Item) -> InventorySlot:
-	#for slot in slots:
-		#if slot.item == item:
-			#return slot
-#
-	#return null
-#
-#func get_number_of_item (item: Item) -> int:
-	#var total = 0
-	#
-	#for slot in slots:
-		#if slot.item == item:
-			#total += slot.quantity
-	#
-	#return total
+func add_item (item: Item):
+	var slot = get_slot_to_add(item)
+	
+	if slot == null:
+		return
+	
+	if slot.item == null:
+		slot.set_item(item)
+	elif slot.item == item:
+		slot.add_item()
+
+func remove_item (item: Item):
+	var slot = get_slot_to_remove(item)
+	
+	if slot == null or slot.item == item:
+		return
+	
+	slot.remove_item()
+
+func get_slot_to_add (item: Item) -> InventorySlot:
+	for slot in slots:
+		if slot.item == item and slot.quantity < item.max_stack_size:
+			return slot
+	for slot in slots:
+		if slot.item == null:
+			return slot
+	
+	return null
+
+func get_slot_to_remove (item: Item) -> InventorySlot:
+	for slot in slots:
+		if slot.item == item:
+			return slot
+	return null
+
+func get_number_of_item (item: Item) -> int:
+	var total = 0
+	
+	for slot in slots:
+		if slot.item == item:
+			total += slot.quantity
+	
+	return total
--- OneDrive - Ouachita Baptist University/Documents/raccoonheist/Characters/inventory_slot.gd
+++ OneDrive - Ouachita Baptist University/Documents/raccoonheist/Characters/inventory_slot.gd
@@ -1,10 +1,10 @@
 class_name InventorySlot
 extends Button
-#var item: Item
-#var quantity: int
-##@onready var icon: TextureRect = get_node("Icon")
-##@onready var quantity_text: Label = get_node("QuantityText")
-#var inventory: Inventory
+var item: Item
+var quantity: int
+@onready var icom: TextureRect = get_node("Icon")
+@onready var quantity_text: Label = get_node("QuantityText")
+var inventory: Inventory
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -14,36 +14,36 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-#func set_item(new_item: Item):
-	#item = new_item
-	#quantity = 1
+func set_item(new_item: Item):
+	item = new_item
+	quantity = 1
 	
-	#if item == null:
-		#icon.visible = false
-	#else:
-		#icon.visible = true
-		#icon.texture = item.icon
-		#
-	#update_quantity_text()
+	if item == null:
+		icom.visible = false
+	else:
+		icom.visible = true
+		icom.texture = item.icom
+		
+	update_quantity_text()
 	
-#func add_item():
-	#quantity += 1
-	#update_quantity_text()	
-#func remove_item():
-	#quantity -= 1
-	#update_quantity_text()
-	#if quantity == 0:
-		#set_item(null)
-#func update_quantity_text():
-	#if quantity <= 1:
-		#quantity_text.text = ""
-	#else:
-		#quantity_text.text = str(quantity)
-		#
+func add_item():
+	quantity += 1
+	update_quantity_text()	
+func remove_item():
+	quantity -= 1
+	update_quantity_text()
+	if quantity == 0:
+		set_item(null)
+func update_quantity_text():
+	if quantity <= 1:
+		quantity_text.text = ""
+	else:
+		quantity_text.text = str(quantity)
+		
 #
-#func _on_pressed():
-	#if item == null:
-		#return
-	#var remove_after_use = item._on_use(inventory.get_parent())
-	#if remove_after_use:
-		#remove_item()
+func _on_pressed():
+	if item == null:
+		return
+	var remove_after_use = item._on_use(inventory.get_parent())
+	if remove_after_use:
+		remove_item()

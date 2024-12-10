e3ed438e3740f12a2205e0414647a5a5ffe3ad7e
Olivia Marine <ogm043@yahoo.com>
2024-12-10 11:11:33 -0600

    I don't really know what changes i made.

--- OneDrive - Ouachita Baptist University/Documents/raccoonheist/part_score_manager.gd
+++ OneDrive - Ouachita Baptist University/Documents/raccoonheist/part_score_manager.gd
@@ -11,4 +11,6 @@ func _process(delta):
 	
 	if score == 5:
 		print("done")
+		TransitionScreen.transition()
+		await TransitionScreen.on_transition_finished
 		get_tree().change_scene_to_file("res://walking_up_to_the_chickenfarm.tscn")
